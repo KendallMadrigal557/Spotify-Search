14e0d4ba067813b082aeae1d6e57fbd352b039f5
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-06 16:06:20 -0600

    reading json to cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/navbar/navbar';
 import SpotifyCard from './components/SpotifyCard/spotifyCard';
+import './App.css'
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/spotify.json');
+        const jsonData = await response.json();
+        setData(jsonData.tracks.items);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      <Navbar/>
-      <SpotifyCard/>
+      <Navbar />
+      <div className='cards-container'>
+        {data.map((song, index) => (
+          <SpotifyCard
+            key={index}
+            imgUrl={song.data.albumOfTrack.coverArt.sources[0].url}
+            NameSong={song.data.name}
+            Artist={song.data.artists.items[0].profile.name}
+            Duration={song.data.duration.totalMilliseconds}
+            Album={song.data.albumOfTrack.name}
+          />
+        ))}
+      </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SpotifyCard/spotifyCard.jsx
+++ src/components/SpotifyCard/spotifyCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import './spotifyCard.css'
 
-const SpotifyCard = () => {
+const SpotifyCard = ({ imgUrl, NameSong, Artist, Duration, Album }) => {
     return (
         <div className="card-container">
             <div className="spotify-card">
                 <div>
-                    <img className="album-image" src="https://i.scdn.co/image/ab67616d00001e02cd2ceb6adaa32ecc83cec5c3" alt="" />
+                    <img className="album-image" src={imgUrl} alt="" />
                 </div>
 
                 <div>
-                    <p id="song-name">Name of song</p>
-                    <p>Artist</p>
-                    <p>Duration</p>
-                    <p>Album</p>
+                    <p id="song-name">{NameSong}</p>
+                    <p>{Artist}</p>
+                    <p>{Duration}</p>
+                    <p>{Album}</p>
                 </div>
             </div>
 
